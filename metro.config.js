95cdd55f894a6adc83043710224fdf3c422bfcab
Owwmann <156894108+Owwmann@users.noreply.github.com>
2026-01-31 22:03:57 -0500

    fix(metro): restore Expo default transformer and add extension support

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,8 @@
 const { getDefaultConfig } = require('expo/metro-config');
+
 const config = getDefaultConfig(__dirname);
 
-config.transformer = {
-  ...config.transformer,
-  babelTransformerPath: require.resolve('metro-react-native-babel-transformer'),
-};
+// Add support for .cjs and .mjs extensions
+config.resolver.sourceExts.push('cjs', 'mjs');
 
 module.exports = config;
