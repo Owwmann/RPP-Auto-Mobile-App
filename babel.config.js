6984fd652619de4a59d97e664d8e7053d87f66b2
klsjrenterprise@gmail.com <klsjrenterprise@gmail.com>
2026-02-13 02:24:38 -0500

    fix: resolve worklets babel plugin and navigation dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,9 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      'babel-preset-expo',
-      '@babel/preset-flow',  // Strip Flow type annotations
-    ],
+    presets: ['babel-preset-expo'],
     plugins: [
-      ['module:react-native-dotenv', {
-        moduleName: '@env',
-        path: '.env',
-        safe: false,
-        allowUndefined: true,
-      }],
+      'react-native-reanimated/plugin',
     ],
   };
 };
